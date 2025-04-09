10823518edf5de74614236d3be1174f9ebc6c8a4
Ilia Vlasov <elijah.vlasov@gmail.com>
2025-04-09 22:14:06 +0100

    Type safe instruction builder

--- src/InstructionBuilder.hs
+++ src/InstructionBuilder.hs
@@ -1,16 +1,20 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeFamilies #-}
 
-module InstructionBuilder () where
+module InstructionBuilder (InstructionType (..), buildInstruction) where
 
 import Data.Bits (shiftR)
 
+import Data.Word (Word32)
+import FenceType (FenceType)
 import Immediate (Immediate)
 import Immediate qualified
 import InstructionBuilder.InstructionChunk
-    ( InstructionChunk
-    , fromWord32
-    )
+  ( InstructionChunk
+  , (>:>)
+  )
+
+import InstructionBuilder.InstructionChunk qualified as IChunk
 import Opcode (Opcode)
 import Opcode qualified
 import Opcode.Funct3 (Funct3)
@@ -21,38 +25,82 @@ import Register (Register)
 import Register qualified
 
 opcode
-    :: Opcode -> InstructionChunk 0 7
-opcode = fromWord32 . Opcode.toWord32
+  :: Opcode -> InstructionChunk 0 7
+opcode = IChunk.fromWord32 . Opcode.toWord32
 
 rd
-    :: Register -> InstructionChunk 7 12
-rd = fromWord32 . Register.toWord32
+  :: Register -> InstructionChunk 7 12
+rd = IChunk.fromWord32 . Register.toWord32
 
 funct3
-    :: Funct3 -> InstructionChunk 12 15
-funct3 = fromWord32 . Funct3.toWord32
+  :: Funct3 -> InstructionChunk 12 15
+funct3 = IChunk.fromWord32 . Funct3.toWord32
 
 rs1
-    :: Register -> InstructionChunk 15 20
-rs1 = fromWord32 . Register.toWord32
+  :: Register -> InstructionChunk 15 20
+rs1 = IChunk.fromWord32 . Register.toWord32
 
 rs2
-    :: Register -> InstructionChunk 20 25
-rs2 = fromWord32 . Register.toWord32
+  :: Register -> InstructionChunk 20 25
+rs2 = IChunk.fromWord32 . Register.toWord32
 
 funct7
-    :: Funct7 -> InstructionChunk 25 32
-funct7 = fromWord32 . Funct7.toWord32
+  :: Funct7 -> InstructionChunk 25 32
+funct7 = IChunk.fromWord32 . Funct7.toWord32
 
-imm_11_0
-    :: Immediate 11 -> InstructionChunk 20 32
-imm_11_0 = fromWord32 . Immediate.toWord32
+data InstructionType
+  = RType
+      { rtRd :: Register
+      , rtRs1 :: Register
+      , rtRs2 :: Register
+      , rtFunct3 :: Funct3
+      , rtFunct7 :: Funct7
+      }
+  | IType
+      { itOpcode :: Maybe Opcode
+      , itRd :: Register
+      , itRs1 :: Register
+      , itImm :: Immediate 12
+      , itFunct3 :: Funct3
+      }
+  | SType
+      { stOpcode :: Opcode
+      , stRs1 :: Register
+      , stRs2 :: Register
+      , stImm :: Immediate 12
+      , stFunct3 :: Funct3
+      }
+  | BType
+      { btRs1 :: Register
+      , btRs2 :: Register
+      , btImm :: Immediate 12
+      , btFunct3 :: Funct3
+      }
+  | UType
+      { utOpcode :: Opcode
+      , utRd :: Register
+      , utImm :: Immediate 20
+      }
+  | JType
+      { jtOpcode :: Opcode
+      , jtRd :: Register
+      , jtImm :: Immediate 20
+      }
+  | FType
+      { ftOpcode :: Opcode
+      , ftFunct3 :: Funct3
+      , ftPre :: FenceType
+      , ftSucc :: FenceType
+      }
 
-imm_4_0
-    :: Immediate 11 -> InstructionChunk 7 12
-imm_4_0 = fromWord32 . Immediate.toWord32
-
-imm_11_5
-    :: Immediate 11 -> InstructionChunk 25 32
-imm_11_5 x = fromWord32 $ Immediate.toWord32 x `shiftR` 5
+buildInstruction
+  :: InstructionType -> Word32
+buildInstruction (RType rtRd rtRs1 rtRs2 rtFunct3 rtFunct7) =
+  IChunk.toWord32
+    $ opcode Opcode.arithRegRegOpcode
+      >:> rd rtRd
+      >:> funct3 rtFunct3
+      >:> rs1 rtRs1
+      >:> rs2 rtRs2
+      >:> funct7 rtFunct7
 
--- src/InstructionBuilder/InstructionChunk.hs
+++ src/InstructionBuilder/InstructionChunk.hs
@@ -2,33 +2,44 @@
 {-# LANGUAGE TypeFamilies #-}
 
 module InstructionBuilder.InstructionChunk
-    ( InstructionChunk
-    , fromWord32
-    , (>:>)
-    ) where
+  ( InstructionChunk
+  , fromWord32
+  , toWord32
+  , writeByte
+  , (>:>)
+  ) where
 
 import Data.Bits (Bits ((.&.), (.|.)), shiftL)
 import Data.Data (Proxy (..))
 import Data.Word (Word32)
-import GHC.TypeLits (CmpNat, KnownNat, Nat, natVal)
+import GHC.TypeLits (CmpNat, KnownNat, Nat, natVal, type (+))
 
 newtype InstructionChunk (l :: Nat) (r :: Nat)
-    = InstructionChunk Word32
+  = InstructionChunk {unwrap :: Word32}
 
 fromWord32
-    :: forall l r
-     . (KnownNat l, KnownNat r, CmpNat l r ~ 'LT)
-    => Word32 -> InstructionChunk l r
+  :: forall l r
+   . (KnownNat l, KnownNat r, CmpNat l r ~ 'LT)
+  => Word32 -> InstructionChunk l r
 fromWord32 x =
-    InstructionChunk
-        $ let lInt = (fromIntegral $ natVal (Proxy @l)) :: Int
-              rInt = (fromIntegral $ natVal (Proxy @r)) :: Int
-              bitsize = rInt - lInt - 1
-              mask = ((1 :: Word32) `shiftL` bitsize) - 1
-          in  (x .&. mask) `shiftL` lInt
+  InstructionChunk
+    $ let lInt = (fromIntegral $ natVal (Proxy @l)) :: Int
+          rInt = (fromIntegral $ natVal (Proxy @r)) :: Int
+          bitsize = rInt - lInt - 1
+          mask = ((1 :: Word32) `shiftL` bitsize) - 1
+      in  (x .&. mask) `shiftL` lInt
+
+toWord32 :: InstructionChunk 0 32 -> Word32
+toWord32 = unwrap
 
 (>:>)
-    :: (KnownNat l, KnownNat m, KnownNat r)
-    => InstructionChunk l m -> InstructionChunk m r -> InstructionChunk l r
+  :: (KnownNat l, KnownNat m, KnownNat r)
+  => InstructionChunk l m -> InstructionChunk m r -> InstructionChunk l r
 (>:>) (InstructionChunk a) (InstructionChunk b) = InstructionChunk (a .|. b)
+
+writeByte
+  :: forall n
+   . (KnownNat n, KnownNat (n + 1), CmpNat n (n + 1) ~ 'LT)
+  => Word32 -> InstructionChunk n (n + 1)
+writeByte = fromWord32
 
